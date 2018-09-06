e7fc0e67ed6952c9e7fcd465c73dc54c54a30395
KNOWN-UNIVERSE\MIAPATA <miapata@uat.edu>
2018-09-06 09:21:14 -0700

    Commit

---
+++ MileStone1/Assets/Scripts/Tank.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour
+{
+    public GameObject camera;
+    public Transform launcher;
+    public float rotateSpeed;
+    public float moveSpeed;
+	public float vertical;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateCannon();
+        Move();
+    }
+
+
+    void RotateCannon()
+    {
+
+    }
+
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.down * rotateSpeed, Space.Self);
+
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up * rotateSpeed, Space.Self);
+
+        }
+
+		vertical= Input.GetAxis("Vertical");
+    
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0, 0, vertical*moveSpeed*Time.deltaTime);
+        }
+		
+    }
+}
